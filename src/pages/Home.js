4f0efd16fa2c650ddec34feac57a5893440a7b8b
katcui <kcui2014@gmail.com>
2024-03-18 22:46:59 -0500

    header and homepage completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      Hello World
+    <div className='home'>
+      <h1>HOME</h1>
+      <div className='topbuttons'>
+        <Link to="/additem">
+          <button className='additembutton'>ADD ITEM</button>
+        </Link>
+        <Link to="/checkout">
+          <button className='checkoutbutton'>CHECK OUT</button>
+        </Link>
+      </div>
+      <div className='bottombutton'>
+        <Link to="/viewinventory">
+          <button className='viewinventorybutton'>VIEW INVENTORY</button>
+        </Link>
+      </div>
     </div>
   )
 }
